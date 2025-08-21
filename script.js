63887a59e20fa264d6bb4a77fec907d06396f555
Nutan Arjun Shinde <shindenutan3714@gmail.com>
2025-08-21 22:00:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) { 
         e.preventDefault();  
         
-        const targetId = this.getAttribute('href');
+        const targetId = this.getAttribute('href'); 
         const targetElement =  document.querySelector(targetId);
         
         if (targetElement) { 
