7f932720d6fcf7d749be78fe3e9edd4b5597eaef
Nutan Arjun Shinde <shindenutan3714@gmail.com>
2025-08-24 20:41:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const mobileMenuBtn = document.querySelector('.mobile-menu-btn');
 const navLinks = document.querySelector('.nav-links'); 
   
-mobileMenuBtn.addEventListener('click', () => {
+mobileMenuBtn.addEventListener('click', () => { 
     navLinks.classList.toggle('active');
 }); 
 
